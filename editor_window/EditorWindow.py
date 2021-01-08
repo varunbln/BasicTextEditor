40745f43abcc7a81193694c783da0295ce660852
Heisenburger69 <apkforgss@gmail.com>
2021-01-08 21:05:02 +0530

    implement a scrollbar
    + additional refactoring

--- editor_window/EditorWindow.py
+++ editor_window/EditorWindow.py
@@ -4,8 +4,9 @@ from utils import io_utils
 
 class EditorWindow:
     root = tk.Tk("Basic Text Editor")
-    text = tk.Text(root)
-    button = tk.Button(root, text="Save")
+    buttonframe = tk.Frame(root)
+    scrollbar = tk.Scrollbar(root)
+    text = tk.Text(root, yscrollcommand=scrollbar.set)
     file_name = None
 
     def __init__(self):
@@ -15,9 +16,22 @@ class EditorWindow:
         self.root.mainloop()
 
     def populate(self):
-        self.text.grid()
-        self.button.configure(text="Save", command=lambda: io_utils.save(self))
-        self.button.grid()
+        self.populate_navbar()
+        self.init_scrollbar()
+        self.configure_textbox()
 
     def register_shortcuts(self):
         self.root.bind('<Control-s>', (lambda event: io_utils.save(self)))
+
+    def populate_navbar(self):
+        self.buttonframe.pack(side=tk.TOP, fill=tk.Y)
+        tk.Button(self.buttonframe, height=2, width=10, text="Save", command=lambda: io_utils.save(self)).grid(row=0, column=0)
+        tk.Button(self.buttonframe, height=2, width=10, text="Find").grid(row=0, column=1)
+
+    def init_scrollbar(self):
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.scrollbar.config(command=self.text.yview)
+
+    def configure_textbox(self):
+        self.text.configure(font=("Ariel", 14))
+        self.text.pack(fill=tk.BOTH, expand=True)
--- utils/io_utils.py
+++ utils/io_utils.py
@@ -7,9 +7,11 @@ def save(editor_window):
         return
     save_loc = editor_window.file_name
     if save_loc is None:
-        save_loc = filedialog.asksaveasfilename(defaultextension='.txt', filetypes=[("All Files (*.*)", '*.*')], title="Save File")
+        save_loc = filedialog.asksaveasfilename(defaultextension='.txt', filetypes=[("All Files (*.*)", '*.*')],
+                                                title="Save File")
     if save_loc == "":
         return
     my_file = open(save_loc, "w+")
     my_file.write(typed_text)
     my_file.close()
+    editor_window.file_name = save_loc
